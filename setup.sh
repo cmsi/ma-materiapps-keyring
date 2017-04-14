6c8f33439836687a28a52c54556e2fd57d4657b3
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2017-04-14 11:36:51 +0900

    Updated for jessie

--- setup.sh
+++ setup.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
+. $(dirname $0)/path.sh
+test -z $BUILD_DIR && exit 127
 
-SCRIPT_DIR=$(dirname $0)
-VERSION=$(sh $SCRIPT_DIR/version.sh)
-VERSION_BASE=$(echo $VERSION | sed 's/-/ /g' | awk '{print $1}')
-BUILD_DIR="materiapps-keyring_$VERSION_BASE"
-
-echo "VERSION: $VERSION"
-echo "VERSION_BASE: $VERSION_BASE"
-echo "SCRIPT_DIR: $SCRIPT_DIR"
-echo "BUILD_DIR: $BUILD_DIR"
-
-if [ -d "$BUILD_DIR" ]; then
-  echo "Error: $BUILD_DIR already exists"
-  exit 127
-fi
-
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
 set -x
 
-mkdir -p $BUILD_DIR 
 cp -frp $SCRIPT_DIR/debian $SCRIPT_DIR/materiapps.gpg $BUILD_DIR
-cd $BUILD_DIR
-dch --release "" --distribution $(lsb_release -s -c) --force-distribution
