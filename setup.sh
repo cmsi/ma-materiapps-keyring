9e99050de873a3aa6ebc203df891fefc04044e7c
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-03-20 21:44:14 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,14 +1,1 @@
-#!/bin/sh
-. $(dirname $0)/path.sh
-test -z $BUILD_DIR && exit 127
-
-rm -rf $BUILD_DIR
-set -x
-
-mkdir -p $BUILD_DIR
-cp -frp $SCRIPT_DIR/debian $SCRIPT_DIR/materiapps.gpg $BUILD_DIR
-
-cd $BUILD_DIR
-sudo apt-get update
-sudo apt-get -y upgrade
-dpkg-checkbuilddeps 2>&1 | sed 's/dpkg-checkbuilddeps.*dependencies: //' | xargs sudo apt-get -y install
+../ma-common/script/setup-nosrc.sh
